9bfde1e7715345c308b88181da00a62debff4368
NuriaCeballos <restxindoki@hotmail.com>
2022-10-01 11:46:51 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatIconModule} from '@angular/material/icon';
+import { MaterialModule } from './material/material.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MaterialModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
